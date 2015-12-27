e794e98d9258b98727012fa8e653ff18c3b7c9d9
Eddie Jaoude <eddie@jaoudestudios.com>
2015-12-27 07:42:45 +0000

    [#24] Removed unused imports

--- src/main/java/com/empowerhack/family/memberservice/FamilyMemberServiceApplication.java
+++ src/main/java/com/empowerhack/family/memberservice/FamilyMemberServiceApplication.java
@@ -1,16 +1,7 @@
 package com.empowerhack.family.memberservice;
 
-import com.empowerhack.family.memberservice.entity.Interest;
-import com.empowerhack.family.memberservice.entity.Member;
-import com.empowerhack.family.memberservice.entity.Skill;
-import com.empowerhack.family.memberservice.entity.Url;
-import com.empowerhack.family.memberservice.repository.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
 public class FamilyMemberServiceApplication {
