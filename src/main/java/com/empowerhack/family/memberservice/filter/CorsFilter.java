ea1183cc3b41e754041e966ef5184e67bd0c895b
Eddie Jaoude <eddie@jaoudestudios.com>
2015-12-28 13:42:37 +0000

    [#18] Added CORS headers- probably need tweaking shortly

---
+++ src/main/java/com/empowerhack/family/memberservice/filter/CorsFilter.java
@@ -0,0 +1,32 @@
+package com.empowerhack.family.memberservice.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public final class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
