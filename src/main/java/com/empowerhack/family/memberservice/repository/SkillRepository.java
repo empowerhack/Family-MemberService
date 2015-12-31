770dc2d7d4c32a8804c94de5d8173f1432fa1889
Eddie Jaoude <eddie@jaoudestudios.com>
2015-12-31 07:14:22 +0000

    [#4] Repositories extend RevisionRepository for envers functionality

--- src/main/java/com/empowerhack/family/memberservice/repository/SkillRepository.java
+++ src/main/java/com/empowerhack/family/memberservice/repository/SkillRepository.java
@@ -2,6 +2,7 @@ package com.empowerhack.family.memberservice.repository;
 
 import com.empowerhack.family.memberservice.domain.Skill;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 
-public interface SkillRepository extends PagingAndSortingRepository<Skill, Long> {
+public interface SkillRepository extends PagingAndSortingRepository<Skill, Long>, RevisionRepository<Skill, Long, Integer> {
 }
--- src/main/java/com/empowerhack/family/memberservice/repository/UrlRepository.java
+++ src/main/java/com/empowerhack/family/memberservice/repository/UrlRepository.java
@@ -2,6 +2,7 @@ package com.empowerhack.family.memberservice.repository;
 
 import com.empowerhack.family.memberservice.domain.Url;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.history.RevisionRepository;
 
-public interface UrlRepository extends PagingAndSortingRepository<Url, Long> {
+public interface UrlRepository extends PagingAndSortingRepository<Url, Long>, RevisionRepository<Url, Long, Integer> {
 }
