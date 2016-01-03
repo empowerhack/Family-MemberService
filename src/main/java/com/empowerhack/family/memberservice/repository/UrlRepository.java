09140523357e788f97ce8dc3472e860ce7330ebd
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-03 16:58:28 +0000

    [#34] Removed un-required Repositories

--- src/main/java/com/empowerhack/family/memberservice/repository/UrlRepository.java
+++
@@ -1,8 +0,0 @@
-package com.empowerhack.family.memberservice.repository;
-
-import com.empowerhack.family.memberservice.domain.Url;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.history.RevisionRepository;
-
-public interface UrlRepository extends PagingAndSortingRepository<Url, Long>, RevisionRepository<Url, Long, Integer> {
-}
