3fb5efb2cd4aefca3df8704e1b1aee79b2f77375
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-08 08:43:08 +0000

    Updated allowed headers in CORS filter

--- src/main/java/com/empowerhack/family/memberservice/filter/CorsFilter.java
+++ src/main/java/com/empowerhack/family/memberservice/filter/CorsFilter.java
@@ -21,7 +21,7 @@ public final class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "application/json");
 
         chain.doFilter(req, resp);
     }
